c0566d7e0d81ca30fba19c495f73048ef563777c
krishan.sharma <krishan.sharma@pathinfotech.com>
2019-06-24 16:40:42 +0530

    checkins pending checkouts

--- src/app/Components/encryption-descryption-sample/encryption-descryption-sample.component.ts
+++ src/app/Components/encryption-descryption-sample/encryption-descryption-sample.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as CryptoJS from 'crypto-js';  
+import * as CryptoJS from 'crypto-js';
 @Component({
   selector: 'app-encryption-descryption-sample',
   templateUrl: './encryption-descryption-sample.component.html',
@@ -7,28 +7,30 @@ import * as CryptoJS from 'crypto-js';
 })
 export class EncryptionDescryptionSampleComponent implements OnInit {
 
-  title = 'EncryptionDecryptionSample';  
-    
-  plainText:string;  
-  encryptText: string;  
-  encPassword: string;  
-  decPassword:string;  
-  conversionEncryptOutput: string;  
-  conversionDecryptOutput:string;  
-    
-  constructor() {  
-  }  
-  //method is used to encrypt and decrypt the text  
-  convertText(conversion:string) {  
-      if (conversion=="encrypt") {  
-        this.conversionEncryptOutput = CryptoJS.AES.encrypt(this.plainText.trim(), this.encPassword.trim()).toString();  
-      }  
-      else {  
-        this.conversionDecryptOutput = CryptoJS.AES.decrypt(this.encryptText.trim(), this.decPassword.trim()).toString(CryptoJS.enc.Utf8);  
-       
-    }  
-  }  
+  title = 'EncryptionDecryptionSample';
 
+  plainText: string;
+  encryptText: string;
+  encPassword: string;
+  decPassword: string;
+  conversionEncryptOutput: string;
+  conversionDecryptOutput: string;
+  conversionEncryptOutput1: string;
+  conversionDecryptOutput1: string;
+
+  constructor() {
+  }
+  // method is used to encrypt and decrypt the text 
+  convertText(conversion: string) {
+    if (conversion === 'encrypt') { 
+      this.conversionEncryptOutput = CryptoJS.AES.encrypt(this.plainText.trim(), 'x').toString();
+      this.conversionEncryptOutput1 = CryptoJS.AES.encrypt(this.encPassword.trim(), 'x').toString();
+    } else {
+      this.conversionDecryptOutput = CryptoJS.AES.decrypt(this.encryptText.trim(), 'x').toString(CryptoJS.enc.Utf8);
+      this.conversionDecryptOutput1 = CryptoJS.AES.decrypt(this.decPassword.trim(), 'x').toString(CryptoJS.enc.Utf8);
+
+    }
+  }
   ngOnInit() {
   }
 
